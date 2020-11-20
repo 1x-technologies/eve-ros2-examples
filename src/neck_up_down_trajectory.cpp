5cc076a918ad7e3ace566b6337f7c72454b5d224
micko-plz <mcl.prcl@gmail.com>
2020-11-20 09:14:46 +0100

    restore old traj, add new ones

--- src/neck_up_down_trajectory.cpp
+++ src/neck_up_down_trajectory.cpp
@@ -20,18 +20,12 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "builtin_interfaces/msg/duration.hpp"
-#include "geometry_msgs/msg/pose.hpp"
-#include "geometry_msgs/msg/point.hpp"
-#include "geometry_msgs/msg/quaternion.hpp"
 #include "action_msgs/msg/goal_status.hpp"
-#include "tf2/LinearMath/Quaternion.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "unique_identifier_msgs/msg/uuid.hpp"
 #include "halodi_msgs/msg/whole_body_trajectory.hpp"
 #include "halodi_msgs/msg/reference_frame_name.hpp"
 #include "halodi_msgs/msg/trajectory_interpolation.hpp"
 #include "halodi_msgs/msg/whole_body_trajectory_point.hpp"
-#include "halodi_msgs/msg/task_space_command.hpp"
 
 using namespace std::chrono_literals;
 using namespace halodi_msgs::msg;
@@ -93,53 +87,42 @@ private:
     trajectory_msg.interpolation_mode.value = TrajectoryInterpolation::MINIMUM_JERK_CONSTRAINED;
     trajectory_msg.trajectory_id = uuid_msg;
 
-    //Add targets for hand motions to pick up a box
-    add_hand_target(&trajectory_msg, 1, 0.25, 0.15, 0.25, 0.0, -pi_/2.0, 0.0, ReferenceFrameName::LEFT_HAND);
-    add_hand_target(&trajectory_msg, 2, 0.25, 0.15, 0.0,  0.0, -pi_/2.0, 0.0, ReferenceFrameName::LEFT_HAND);
-    add_hand_target(&trajectory_msg, 3, 0.25, 0.4,  0.0,  0.0, -pi_/2.0, 0.0, ReferenceFrameName::LEFT_HAND);
-    add_hand_target(&trajectory_msg, 4, 0.25, 0.4,  0.25, 0.0, -pi_/2.0, 0.0, ReferenceFrameName::LEFT_HAND);
-    add_hand_target(&trajectory_msg, 5, 0.25, 0.15, 0.25, 0.0, -pi_/2.0, 0.0, ReferenceFrameName::LEFT_HAND);
+    // neck limits lower="-0.335" upper="0.506" 
+    std::vector<double> neck_angles = {-0.33, 0.0, 0.5};
+    std::vector<double> neck_timing = {2, 4, 6};
+    
+    for (std::vector<double>::size_type i = 0; i < neck_angles.size(); ++i) {
+      
+      WholeBodyTrajectoryPoint msg;
+      builtin_interfaces::msg::Duration duration;
+      duration.sec = neck_timing.at(i);
+      msg.time_from_start = duration;
+      msg.joint_space_commands.push_back(generate_joint_space_command(JointName::NECK_PITCH, neck_angles.at(i))); 
+      trajectory_msg.trajectory_points.push_back(msg);
+
+    }
 
     RCLCPP_INFO(this->get_logger(), "Sending trajectory, listening for whole_body_trajectory_status...");
     publisher_->publish(trajectory_msg);
 
   }
 
-  void add_hand_target(WholeBodyTrajectory * trajectory, int32_t t, double x, double y, double z, double yaw, double pitch, double roll, ReferenceFrameName::_frame_id_type frame) const
+  /*
+  This generates the individual single joint command
+  */
+  JointSpaceCommand generate_joint_space_command(int32_t joint_id, double q_des, double qd_des = 0.0, double qdd_des = 0.0) const
   {
-    WholeBodyTrajectoryPoint target;
-    TaskSpaceCommand hand_command;
+    JointSpaceCommand ret_msg;
+    JointName name;
+    name.joint_id = joint_id;
+    ret_msg.joint = name;
+    ret_msg.q_desired = q_des;
+    ret_msg.qd_desired = qd_des;
+    ret_msg.qdd_desired = qdd_des;
+    ret_msg.use_default_gains = true;
+    return ret_msg;
+  }  
 
-    //We specify the link frame to move and what reference frame the target pose will be described in here
-    ReferenceFrameName::Type body_frame, reference_frame;
-    body_frame.frame_id = frame;
-    reference_frame.frame_id = ReferenceFrameName::PELVIS;
-    hand_command.body_frame = body_frame;
-    hand_command.expressed_in_frame = reference_frame;
-    hand_command.express_in_z_up = true;
-
-    //Setting a pose requires an x,y,z point and an orientation in the form of a quaternion
-    geometry_msgs::msg::Pose pose;
-    geometry_msgs::msg::Point point;
-    point.x = x; point.y = y; point.z = z;
-    geometry_msgs::msg::Quaternion quat_msg;
-    tf2::Quaternion quat_tf;
-    quat_tf.setRPY(roll, pitch, yaw);
-    quat_msg = tf2::toMsg(quat_tf);
-    pose.position = point; pose.orientation = quat_msg;
-    hand_command.pose = pose;
-
-    //We specify the desired time at which the hand should be at its target. Time is seconds after the start of the trajectory
-    builtin_interfaces::msg::Duration duration;
-    duration.sec = t;
-    target.time_from_start = duration;
-
-    //Add the hand command to the list of targets, add the target list to the trajectory points
-    target.task_space_commands.push_back(hand_command);
-    trajectory->trajectory_points.push_back(target);
-  }
-
-  const double pi_ = boost::math::constants::pi<double>();
   rclcpp::Publisher<WholeBodyTrajectory>::SharedPtr publisher_;
   rclcpp::Subscription<action_msgs::msg::GoalStatus>::SharedPtr subscription_;
   mutable unique_identifier_msgs::msg::UUID uuid_msg_;
